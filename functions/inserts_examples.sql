60b12bfe5f8a47b718d70aa5e6d1b67715ac3594
elzacodecool <elliegolec@gmail.com>
2018-10-02 11:51:39 +0200

    Add inserts

--- functions/inserts_examples.sql
+++ functions/inserts_examples.sql
@@ -1,1 +1,14 @@
 INSERT INTO categories(name) VALUES ('jazz');
+
+INSERT INTO listeners(name, login, password, register_date)
+  VALUES ('Eliza', 'eliza', 'password', now());
+
+INSERT INTO playlists(name, id_owner)
+  VALUES ('name', 1);
+
+INSERT INTO playlists_listeners VALUES (1,1);
+
+INSERT INTO premium_accounts(id_listener, open_date, termination_date)
+  VALUES (1, now(), now() + interval '1 month');
+
+INSERT INTO playlists_songs VALUES (1, 1);

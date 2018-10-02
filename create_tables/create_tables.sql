2d44ad4b90313ca7c902cb5e1d8dfb2e949abb9d
elzacodecool <elliegolec@gmail.com>
2018-10-02 11:22:29 +0200

    Add queries to create tables

--- create_tables/create_tables.sql
+++ create_tables/create_tables.sql
@@ -8,3 +8,38 @@ CREATE TABLE songs (
   name VARCHAR(255),
   id_category INTEGER REFERENCES categories(id_category)
 );
+
+
+CREATE TABLE listeners (
+  id_listener SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  login VARCHAR(255) UNIQUE,
+  password VARCHAR(255),
+  register_date TIMESTAMP
+);
+
+CREATE TABLE premium_accounts (
+  id_premium SERIAL PRIMARY KEY,
+  id_listener INT REFERENCES listeners(id_listener),
+  open_date TIMESTAMP,
+  termination_date TIMESTAMP
+);
+
+CREATE TABLE playlists (
+  id_playlist SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  id_owner INT REFERENCES listeners(id_listener)
+);
+
+CREATE TABLE playlists_listeners (
+  id_playlist INT REFERENCES playlists(id_playlist),
+  id_listener INT REFERENCES listeners(id_listener),
+  PRIMARY KEY (id_playlist, id_listener)
+);
+
+CREATE TABLE playlists_songs (
+  id_playlist INT REFERENCES playlists(id_playlist),
+  id_song INT REFERENCES songs(id_song),
+  PRIMARY KEY (id_playlist, id_song)
+);
+

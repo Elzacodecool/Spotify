e40a480cbfe654858904f424c85ddebcef217364
elzacodecool <elliegolec@gmail.com>
2018-10-02 15:44:38 +0200

    Add generate premium

--- example_data/generate_from_files.sql
+++ example_data/generate_from_files.sql
@@ -1,2 +1,6 @@
 COPY listeners(name, login, password, register_date)
-  FROM 'listeners.csv' DELIMITER ',' CSV HEADER;
+  FROM 'C:\Users\Eliza\codecool\Spotify\example_data\listeners.csv' DELIMITER ',' CSV HEADER;
+
+COPY premium_accounts(id_listener, open_date, termination_date)
+  FROM 'C:\Users\Eliza\codecool\Spotify\example_data\premium_accounts.csv' DELIMITER ',' CSV HEADER;
+

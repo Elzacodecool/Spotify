aa0ec18384bbfd537003b8c98b615fac7cb26ca0
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-10-02 11:47:51 +0200

    Basic inserts

--- functions/inserts_examples.sql
+++ functions/inserts_examples.sql
@@ -1,1 +1,16 @@
 INSERT INTO categories(name) VALUES ('jazz');
+
+INSERT INTO songs(name, id_category) VALUES ('Happy Birthday', 1);
+
+INSERT INTO artists(name) VALUES ('Sheila Sue');
+
+INSERT INTO albums(name) VALUES ('Hello');
+
+INSERT INTO albums_artist VALUES(1,1);
+
+INSERT INTO albums_songs VALUES(1,1);
+
+INSERT INTO songs_artist VALUES(1,1);
+
+
+

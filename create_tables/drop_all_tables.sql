95f64c98e1b7e050587d0a40026ad745c1c29299
elzacodecool <elliegolec@gmail.com>
2018-10-02 12:13:16 +0200

    Add script to delete all tables

---
+++ create_tables/drop_all_tables.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS playlists_songs,
+  playlists_listeners,
+  playlists,
+  listeners,
+  premium_accounts,
+  songs_artist,
+  albums_songs,
+  albums_artist,
+  albums,
+  artists,
+  songs,
+  categories
+;

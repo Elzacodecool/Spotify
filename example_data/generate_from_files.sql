fb4585f5a6325cefffab6d2b20bad1dd635cfe99
elzacodecool <elliegolec@gmail.com>
2018-10-02 15:52:45 +0200

    Add generated playlists

--- example_data/generate_from_files.sql
+++ example_data/generate_from_files.sql
@@ -4,3 +4,5 @@ COPY listeners(name, login, password, register_date)
 COPY premium_accounts(id_listener, open_date, termination_date)
   FROM 'C:\Users\Eliza\codecool\Spotify\example_data\premium_accounts.csv' DELIMITER ',' CSV HEADER;
 
+COPY playlists(name, id_owner)
+  FROM 'C:\Users\Eliza\codecool\Spotify\example_data\playlists.csv' DELIMITER ',' CSV HEADER;

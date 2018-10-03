8150c0bb51e392cfa81214bba4242e423bb6b3c0
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-10-03 11:01:22 +0200

    Set trigger after inserting into playlists

---
+++ functions/trigger_for_playlists_listeners.sql
@@ -0,0 +1,16 @@
+CREATE TRIGGER play_list_update
+  AFTER INSERT
+  ON playlists
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_playlists_listeners();
+
+CREATE OR REPLACE FUNCTION update_playlists_listeners()
+  RETURNS trigger AS
+$$
+BEGIN
+ INSERT INTO playlists_listeners(id_playlist, id_listener)
+ VALUES(NEW.id_playlist,NEW.id_owner);
+ RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;

a2b595d4f6d92b2d96df1663d85df9fc76e8aadc
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-10-03 09:27:14 +0200

    Create tables for artist albums

--- example_data/generate_from_files.sql
+++ example_data/generate_from_files.sql
@@ -1,2 +1,11 @@
 COPY listeners(name, login, password, register_date)
   FROM 'listeners.csv' DELIMITER ',' CSV HEADER;
+
+COPY categories(name)
+FROM '/Users/elzbietakrzych/Documents/codecool/ADVANCED/2018_10_01_TW/Spotify/example_data/categories.csv' DELIMITER ',' CSV HEADER;
+
+COPY artists(name)
+FROM '/Users/elzbietakrzych/Documents/codecool/ADVANCED/2018_10_01_TW/Spotify/example_data/artists.csv' DELIMITER ',' CSV HEADER;
+
+COPY albums(name)
+FROM '/Users/elzbietakrzych/Documents/codecool/ADVANCED/2018_10_01_TW/Spotify/example_data/albums.csv' DELIMITER ',' CSV HEADER;
--- example_data/read_category.sql
+++
@@ -1,2 +0,0 @@
-COPY categories(name)
-FROM '/Users/elzbietakrzych/Documents/codecool/ADVANCED/2018_10_01_TW/Spotify/example_data/categories.csv' DELIMITER ',' CSV HEADER;

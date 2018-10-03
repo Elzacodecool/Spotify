1e2a5f90ba311f3407b6db9592cc265d43e978fa
elzacodecool <elliegolec@gmail.com>
2018-10-03 13:09:00 +0200

    Add get songs from album

--- functions/search.sql
+++ functions/search.sql
@@ -29,6 +29,29 @@ BEGIN
 end;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION get_song_from_album(id_displayed_album INT)
+  RETURNS TABLE (
+  id_song INT,
+  name VARCHAR(255),
+  id_category INT
+  ) AS $$
+BEGIN
+  RETURN QUERY SELECT
+  songs.id_song,
+  songs.name,
+  songs.id_category
+  FROM
+       albums_songs
+  JOIN
+      songs
+      ON albums_songs.id_song = songs.id_song
+  WHERE albums_songs.id_album = id_displayed_album;
+
+end;
+$$ LANGUAGE plpgsql;
+
+
 
 SELECT * FROM search_by_phrase('H');
+SELECT * FROM get_song_from_album(1);
 

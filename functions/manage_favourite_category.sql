6d8f32a40e40b7ab674469a24361615178aafc23
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-10-04 13:16:57 +0200

    Implement favourite category

--- functions/manage_favourite_category.sql
+++ functions/manage_favourite_category.sql
@@ -17,4 +17,43 @@ END;
 $$
 LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION list_songs_from_favourite_category(favourite_category_id INT)
+  RETURNS TABLE (
+    song_name VARCHAR(255),
+    album_name VARCHAR(255),
+    artist_name VARCHAR(255)) AS
+  $$
+  BEGIN
+    RETURN QUERY SELECT songs.name, albums.name, artists.name FROM songs
+    INNER JOIN albums_songs on songs.id_song = albums_songs.id_song
+    INNER JOIN songs_artists ON songs.id_song = songs_artists.id_song
+    INNER JOIN albums on albums_songs.id_album = albums.id_album
+    INNER JOIN artists on songs_artists.id_artist = artists.id_artist
+    WHERE songs.id_category = favourite_category_id ORDER BY RANDOM() LIMIT 10;
+  END;
+  $$
+  LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION list_songs_from_favourite_category()
+  RETURNS TABLE (
+    song_name VARCHAR(255),
+    album_name VARCHAR(255),
+    artist_name VARCHAR(255)) AS
+  $$
+  BEGIN
+    RETURN QUERY SELECT songs.name, albums.name, artists.name FROM songs
+    INNER JOIN albums_songs on songs.id_song = albums_songs.id_song
+    INNER JOIN songs_artists ON songs.id_song = songs_artists.id_song
+    INNER JOIN albums on albums_songs.id_album = albums.id_album
+    INNER JOIN artists on songs_artists.id_artist = artists.id_artist
+    ORDER BY RANDOM() LIMIT 10;
+  END;
+  $$
+  LANGUAGE plpgsql;
+
+
 SELECT find_favourite_category(12);
+
+SELECT list_songs_from_favourite_category(2);
+
+SELECT list_songs_from_favourite_category();
